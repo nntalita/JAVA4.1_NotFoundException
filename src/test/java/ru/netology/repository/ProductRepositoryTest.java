93ebecbdbf0e40d6ca0664e102d84fecf12efade
Natalya Pletneva <nntalita@gmail.com>
2021-11-06 12:52:08 +0300

    initial commit

---
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -0,0 +1,12 @@
+package ru.netology.repository;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepositoryTest {
+
+    @Test
+    void removeById() {
+    }
+}
